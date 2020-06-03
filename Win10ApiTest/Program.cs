ac8a59f3fb1fd5080b454596f6fcb69b9f7e0ac5
xingzheng <xingzheng@hotmail.com>
2020-06-03 18:27:10 +0800

    add library

---
+++ Win10ApiTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation.Metadata;
+
+namespace Win10ApiTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestWin10Api();
+            Console.ReadLine();
+        }
+
+        private static void TestWin10Api()
+        {
+            if (ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 8))
+            {
+                Console.WriteLine("Windows.Foundation.UniversalApiContract 8.0 exists.");
+            }
+            else
+            {
+                Console.WriteLine("Windows.Foundation.UniversalApiContract doen't exist.");
+            }
+        }
+    }
+}
